e0f4f5e913aac020b61e25b137dd35d4e3b2ae54
fcomtz <jose_fko_92@hotmail.com>
2026-05-20 13:55:10 -0600

    change email to username in login

---
+++ app/Filament/Resources/Auth/Login.php
@@ -0,0 +1,26 @@
+<?php
+// app/Filament/Auth/Login.php
+
+namespace App\Filament\Auth;
+
+use Filament\Auth\Pages\Login as BaseLogin;
+use Filament\Forms\Components\TextInput;
+
+class Login extends BaseLogin
+{
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        return [
+            'username' => $data['username'],
+            'password' => $data['password'],
+        ];
+    }
+
+    protected function getEmailFormComponent(): TextInput
+    {
+        return TextInput::make('username')
+            ->label('Usuario')
+            ->required()
+            ->autocomplete();
+    }
+}
